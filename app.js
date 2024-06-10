844a2ed1f13fb55a61ebb441194ccaa65a1b717e
anshul.singhal <singhal.s01@gmail.com>
2024-06-11 00:14:50 +0530

    Added first module

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-function sayHello(name)
-{
-    console.log('Hello ' + name);
-}
+//Every file in node is a Module
+// variable and function defined in a module is local to that module and not availble outside the module
+// until not exported
+var logger = require('./logger.js'); // to include the exported module
+console.log(logger.log("message from app.js"));
+//console.log(module);
+// var sayHello = function(name)
+//                 {
+//                     console.log('Hello ' + name);
+//                 }
 
-sayHello('world');
+// sayHello('world');
